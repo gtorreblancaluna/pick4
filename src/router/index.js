d50a482e863b8ceba6124f0adc698c4b6b1d19f1
Gerardo Torreblanca Luna <gtorreblancaluna@gmail.com>
2020-02-19 21:38:02 -0600

    First commit

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import PlayPick4 from '../views/PlayPick4.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/play-pick4',
+    name: 'play-pick4',
+    component: PlayPick4
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/views/PlayPick4.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="pick4">
+    <h1>Play pick4</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlayPick4',
+  components: {
+
+  }
+}
+</script>
